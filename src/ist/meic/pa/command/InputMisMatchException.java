e926b8630d81984d0d0745875cb1b7f93dd2ee51
franferrer <fmgf17@outlook.com>
2015-03-28 21:00:40 +0000

    Set command generic w/ reflection added.

---
+++ src/ist/meic/pa/command/InputMisMatchException.java
@@ -0,0 +1,13 @@
+package ist.meic.pa.command;
+
+public class InputMisMatchException extends Exception{
+	
+	public InputMisMatchException(){
+		super();
+	}
+	
+	public InputMisMatchException(String message){
+		super(message);
+	}
+	
+}
--- src/ist/meic/pa/command/SetCommand.java
+++ src/ist/meic/pa/command/SetCommand.java
@@ -3,70 +3,72 @@ package ist.meic.pa.command;
 import ist.meic.pa.DebugMonitor;
 import ist.meic.pa.StackEntry;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Stack;
 
 public class SetCommand {
 
-	public static void execute(Throwable t, String[] args) {
+	public static void execute(Throwable t, String[] args)
+			throws InputMisMatchException {
 		Stack<StackEntry> callStack = DebugMonitor.getCallStack();
 		String var = args[0];
 		String value = args[1];
+		Class<?>[] parameterTypeString = { String.class };
+		Class<?>[] parameterTypeChar = { char.class };
 
 		StackEntry top = callStack.lastElement();
 		Object instance = top.getInstance();
 		Field[] fields = instance.getClass().getDeclaredFields();
 		for (Field f : fields) {
 			if (var.equals(f.getName())) {
+
 				f.setAccessible(true);
 				String type = f.getType().getName();
-				if (type == null) {
-					return;
-				}
+				System.out.println("Type: " + type);
 				try {
-					Object obj = null;
-					//System.out.println("Type:" + type);
-					switch (type) {
-					case "double":
-						obj = Double.valueOf(value);
-						break;
-					case "int":
-						obj = Integer.valueOf(value);
-						break;
-					case "long":
-						obj = Long.valueOf(value);
-						break;
-					case "short":
-						obj = Short.valueOf(value);
-						break;
-					case "float":
-						obj = Float.valueOf(value);
-						break;
-					case "boolean":
-						obj = Boolean.valueOf(value);
-						break;
-					case "char":
-						if (value.length() == 1) {
-							obj = value.charAt(0);
-						} else {
-							throw new NumberFormatException();
-						}
-						break;
-					case "java.lang.String":
-						obj = value;
-						break;
-					}
-					f.set(instance, obj);
+					try {
 
-				} catch (NumberFormatException e) {
+						Object typeObj = f.get(instance);
+						Class<?> cl = typeObj.getClass();
+						Constructor<?> ctor;
+						Object obj = null;
+
+						if (type.equals("char")) {
+							ctor = cl.getConstructor(parameterTypeChar);
+							if (value.length() == 1) {
+								obj = ctor.newInstance(value.charAt(0));
+							} else {
+								throw new InputMisMatchException(type);
+							}
+						} else {
+							ctor = cl.getConstructor(parameterTypeString);
+							obj = ctor.newInstance(value);
+						}
+
+						f.set(instance, obj);
+						System.out.println("Value:" + f.get(instance));
+
+					} catch (SecurityException e) {
+						e.printStackTrace();
+					} catch (IllegalArgumentException e) {
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						e.printStackTrace();
+					} catch (NoSuchMethodException e) {
+						e.printStackTrace();
+					} catch (InstantiationException e) {
+						e.printStackTrace();
+					} catch (InvocationTargetException e) {
+						throw new InputMisMatchException();
+					}
+				} catch (InputMisMatchException e) {
 					System.out
-							.println("The value you want to assign is not from type "
-									+ type);
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
+							.println("The value you want to set must be from type "
+									+ f.getType().getName());
 				}
+
 			}
 
 		}
