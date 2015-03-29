6b5be0b27b5288bd4ba19d496e0c5e0a5f3ed44f
fdkio <fdkio@hotmail.com>
2015-03-30 00:53:47 +0100

    Code Cleanup

--- src/ist/meic/pa/DebugMonitor.java
+++ src/ist/meic/pa/DebugMonitor.java
@@ -27,73 +27,6 @@ public class DebugMonitor {
 		callStack.pop();
 	}
 
-	public static Class<?> convertFromWrapperToPrimitive(Class<?> wrapper) {
-		String wName = wrapper.getName();
-		if (wName.equals("java.lang.Integer")) {
-			return int.class;
-		} else if (wName.equals("java.lang.Float")) {
-			return float.class;
-		} else if (wName.equals("java.lang.Double")) {
-			return double.class;
-		} else if (wName.equals("java.lang.Long")) {
-			return long.class;
-		} else if (wName.equals("java.lang.Boolean")) {
-			return boolean.class;
-		} else if (wName.equals("java.lang.Character")) {
-			return char.class;
-		} else if (wName.equals("java.lang.Byte")) {
-			return byte.class;
-		} else if (wName.equals("java.lang.Void")) {
-			return void.class;
-		} else if (wName.equals("java.lang.Short")) {
-			return short.class;
-		} else {
-			return wrapper;
-		}
-	}
-
-	public static boolean isPrimitive(char c) {
-		if (Character.toString(c).equals("Z")
-				|| Character.toString(c).equals("B")
-				|| Character.toString(c).equals("C")
-				|| Character.toString(c).equals("S")
-				|| Character.toString(c).equals("I")
-				|| Character.toString(c).equals("J")
-				|| Character.toString(c).equals("F")
-				|| Character.toString(c).equals("D")
-				|| Character.toString(c).equals("V")) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public static String nextSigType(String sig) {
-		if (isPrimitive(sig.charAt(1))) {
-			StringBuilder sb = new StringBuilder(sig);
-			sb.deleteCharAt(1);
-			return sb.toString();
-		} else if (Character.toString(sig.charAt(1)).equals("L")) {
-			String[] parts = sig.split(";", 2);
-			return "(" + parts[1];
-		} else if (Character.toString(sig.charAt(1)).equals("[")) {
-			/**
-			 * String is like "([[[[IDI..." We want to remove all instances of
-			 * "[" and the next letter also
-			 */
-			while (Character.toString(sig.charAt(1)).equals("[")) {
-				StringBuilder sb = new StringBuilder(sig);
-				sb.deleteCharAt(1);
-				sig = sb.toString();
-			}
-			StringBuilder sb = new StringBuilder(sig);
-			sb.deleteCharAt(1);
-			return sb.toString();
-
-		}
-		return null;
-	}
-
 	public static Object methodCall(Object target, Object[] args,
 			String classToCall, String methodToCall, String signature)
 			throws Throwable {
@@ -103,13 +36,13 @@ public class DebugMonitor {
 		Class<?>[] parameterType = new Class<?>[args.length];
 
 		for (int i = 0; i < args.length; i++) {
-			if (isPrimitive(signature.charAt(1))) {
-				parameterType[i] = convertFromWrapperToPrimitive(args[i]
+			if (Utils.isPrimitive(signature.charAt(1))) {
+				parameterType[i] = Utils.convertFromWrapperToPrimitive(args[i]
 						.getClass());
 			} else {
 				parameterType[i] = args[i].getClass();
 			}
-			s = nextSigType(s);
+			s = Utils.nextSigType(s);
 		}
 
 		try {
@@ -174,40 +107,4 @@ public class DebugMonitor {
 			}
 		} while (true);
 	}
-
-	public static Object returnCmd(Object r, String signature) {
-		Object result = new Object();
-		try {
-			String s = (String) r;
-
-			String[] parts = signature.split("\\)", 2);
-			if (parts[1].equals("Z")) {
-				result = new Boolean(s);
-			} else if (parts[1].equals("B")) {
-				result = new Byte(s);
-			} else if (parts[1].equals("C")) {
-				result = new Character(s.charAt(0));
-			} else if (parts[1].equals("S")) {
-				result = new Short(s);
-			} else if (parts[1].equals("I")) {
-				result = new Integer(s);
-			} else if (parts[1].equals("J")) {
-				result = new Long(s);
-			} else if (parts[1].equals("F")) {
-				result = new Float(s);
-			} else if (parts[1].equals("D")) {
-				result = new Double(s);
-			} else if (parts[1].equals("V")) {
-				result = null;
-			} else {
-
-				// Necessary?
-				result = null;
-			}
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-
 }
--- src/ist/meic/pa/Utils.java
+++ src/ist/meic/pa/Utils.java
@@ -11,4 +11,73 @@ public class Utils {
 			return "";
 		}
 	}
+
+	public static Class<?> convertFromWrapperToPrimitive(Class<?> wrapper) {
+		String wName = wrapper.getName();
+		if (wName.equals("java.lang.Integer")) {
+			return int.class;
+		} else if (wName.equals("java.lang.Float")) {
+			return float.class;
+		} else if (wName.equals("java.lang.Double")) {
+			return double.class;
+		} else if (wName.equals("java.lang.Long")) {
+			return long.class;
+		} else if (wName.equals("java.lang.Boolean")) {
+			return boolean.class;
+		} else if (wName.equals("java.lang.Character")) {
+			return char.class;
+		} else if (wName.equals("java.lang.Byte")) {
+			return byte.class;
+		} else if (wName.equals("java.lang.Void")) {
+			return void.class;
+		} else if (wName.equals("java.lang.Short")) {
+			return short.class;
+		} else {
+			return wrapper;
+		}
+	}
+
+	//TODO REMOVE
+	public static boolean isPrimitive(char c) {
+		if (Character.toString(c).equals("Z")
+				|| Character.toString(c).equals("B")
+				|| Character.toString(c).equals("C")
+				|| Character.toString(c).equals("S")
+				|| Character.toString(c).equals("I")
+				|| Character.toString(c).equals("J")
+				|| Character.toString(c).equals("F")
+				|| Character.toString(c).equals("D")
+				|| Character.toString(c).equals("V")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	//TODO REMOVE AND USE $TYPE
+	public static String nextSigType(String sig) {
+		if (isPrimitive(sig.charAt(1))) {
+			StringBuilder sb = new StringBuilder(sig);
+			sb.deleteCharAt(1);
+			return sb.toString();
+		} else if (Character.toString(sig.charAt(1)).equals("L")) {
+			String[] parts = sig.split(";", 2);
+			return "(" + parts[1];
+		} else if (Character.toString(sig.charAt(1)).equals("[")) {
+			/**
+			 * String is like "([[[[IDI..." We want to remove all instances of
+			 * "[" and the next letter also
+			 */
+			while (Character.toString(sig.charAt(1)).equals("[")) {
+				StringBuilder sb = new StringBuilder(sig);
+				sb.deleteCharAt(1);
+				sig = sb.toString();
+			}
+			StringBuilder sb = new StringBuilder(sig);
+			sb.deleteCharAt(1);
+			return sb.toString();
+
+		}
+		return null;
+	}
 }
