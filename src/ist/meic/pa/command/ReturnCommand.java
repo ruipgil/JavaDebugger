c90a6a82e11d03e9c77783f53c0396578545e02f
Francisco Pedro Durao <ist169533@linbase.rnl.tecnico.ulisboa.pt>
2015-03-30 18:06:56 +0100

    Fixed stuff and thangs

--- src/ist/meic/pa/command/ReturnCommand.java
+++ src/ist/meic/pa/command/ReturnCommand.java
@@ -3,49 +3,63 @@ package ist.meic.pa.command;
 import ist.meic.pa.DebugMonitor;
 import ist.meic.pa.StackEntry;
 
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class ReturnCommand {
+	public static HashMap<String,String> types = new HashMap<String, String>();
+	static { 
+		types.put("boolean", "java.lang.Boolean");
+		types.put("byte", "java.lang.Byte");
+		types.put("char", "java.lang.Character");
+		types.put("short", "java.lang.Short");
+		types.put("int", "java.lang.Integer");
+		types.put("long", "java.lang.Long");
+		types.put("float", "java.lang.Float");
+		types.put("double", "java.lang.Double");
+		types.put("void", "java.lang.Void");
+	}
 	public static Object execute(Throwable t, String[] args) {
 		if(args.length == 0) {
 			return null;
 		}
+		
+		Class<?>[] parameterTypeString = { String.class };
+		Class<?>[] parameterTypeChar = { char.class };
+
 		String r = args[0];
 		Stack<StackEntry> callStack = DebugMonitor.getCallStack();
 		StackEntry top = callStack.lastElement();
-		String signature = top.getSignature() ;
-
-		Object result = new Object();
+		Class<?> resultType = (Class<?>) top.getResultType() ;
+	
+		Object obj = null;
 		try{
-			String s = (String)r;
-
-			String[] parts = signature.split("\\)", 2);
-			if(parts[1].equals("Z")){
-				result = new Boolean(s);
-			}else if (parts[1].equals("B")){
-				result = new Byte(s);
-			}else if (parts[1].equals("C")){
-				result = new Character(s.charAt(0));
-			}else if (parts[1].equals("S")){
-				result = new Short(s);
-			}else if (parts[1].equals("I")){
-				result = new Integer(s);
-			}else if (parts[1].equals("J")){
-				result = new Long(s);
-			}else if (parts[1].equals("F")){
-				result = new Float(s);
-			}else if (parts[1].equals("D")){
-				result = new Double(s);
-			}else if (parts[1].equals("V")) {
-				result = null;
-			}else {
-				
-				//Necessary?
-				result = null;
+			String s = r;
+			String type = types.get(resultType.toString());
+			Class<?> cl = Class.forName(type);
+			
+			Constructor<?> ctor;
+			
+			
+			if (type.equals("Character")) {
+				ctor = cl.getConstructor(parameterTypeChar);
+				if (s.length() == 1) {
+					obj = ctor.newInstance(s.charAt(0));
+				} else {
+					throw new InputMisMatchException(type);
+				}
+			} else {
+				ctor = cl.getConstructor(parameterTypeString);
+				obj = ctor.newInstance(s);
 			}
+			
 		}catch (Throwable e){
-			e.printStackTrace();
+			//e.printStackTrace();
+			System.out.println("Return must be from type " + resultType.toString() );
+			return null;
 		}
-		return result;
+		return obj;
+		
 	}
 }
