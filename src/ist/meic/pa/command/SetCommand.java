11ab68466cc0292fb3b073120c17d666548e8ea9
Francisco Miguel Grilate Ferreira <ist169396@linbase.rnl.tecnico.ulisboa.pt>
2015-03-30 15:29:55 +0100

    Set function corrected: args if

--- src/ist/meic/pa/command/SetCommand.java
+++ src/ist/meic/pa/command/SetCommand.java
@@ -9,70 +9,80 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Stack;
 
 public class SetCommand {
-
+	static Class<?>[] parameterTypeString = { String.class };
+	static Class<?>[] parameterTypeChar = { char.class };
+	
 	public static Object execute(Throwable t, String[] args)
 			throws InputMisMatchException {
+		System.out.println("HERE1");
 		Stack<StackEntry> callStack = DebugMonitor.getCallStack();
-		if(args.length<=2) {
+		if(args.length<2) {
 			return null;
 		}
 		String var = args[0];
 		String value = args[1];
-		Class<?>[] parameterTypeString = { String.class };
-		Class<?>[] parameterTypeChar = { char.class };
-
+		
 		StackEntry top = callStack.lastElement();
 		Object instance = top.getInstance();
 		Field[] fields = instance.getClass().getDeclaredFields();
+		System.out.println("HERE1");
 		for (Field f : fields) {
 			if (var.equals(f.getName())) {
-
-				f.setAccessible(true);
-				String type = f.getType().getName();
 				try {
-					try {
-
-						Object typeObj = f.get(instance);
-						Class<?> cl = typeObj.getClass();
-						Constructor<?> ctor;
-						Object obj = null;
-
-						if (type.equals("char")) {
-							ctor = cl.getConstructor(parameterTypeChar);
-							if (value.length() == 1) {
-								obj = ctor.newInstance(value.charAt(0));
-							} else {
-								throw new InputMisMatchException(type);
-							}
-						} else {
-							ctor = cl.getConstructor(parameterTypeString);
-							obj = ctor.newInstance(value);
-						}
-						f.set(instance, obj);
-						
-					} catch (SecurityException e) {
-						e.printStackTrace();
-					} catch (IllegalArgumentException e) {
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						e.printStackTrace();
-					} catch (NoSuchMethodException e) {
-						e.printStackTrace();
-					} catch (InstantiationException e) {
-						e.printStackTrace();
-					} catch (InvocationTargetException e) {
-						throw new InputMisMatchException();
-					}
+					System.out.println("HERE2");
+					setField(f,instance,value);
+					
 				} catch (InputMisMatchException e) {
 					System.out
 							.println("The value you want to set must be from type "
 									+ f.getType().getName());
 				}
-
 			}
 		
 		}
 		return null;
 	}
+	
+	public static void setField(Field f, Object instance, String value) throws InputMisMatchException{
+		
+		f.setAccessible(true);
+		String type = f.getType().getName();
+		
+			try {
+
+				Object typeObj = f.get(instance);
+				Class<?> cl = typeObj.getClass();
+				Constructor<?> ctor;
+				Object obj = null;
+
+				if (type.equals("char")) {
+					ctor = cl.getConstructor(parameterTypeChar);
+					if (value.length() == 1) {
+						obj = ctor.newInstance(value.charAt(0));
+					} else {
+						throw new InputMisMatchException(type);
+					}
+				} else {
+					ctor = cl.getConstructor(parameterTypeString);
+					obj = ctor.newInstance(value);
+				}
+				f.set(instance, obj);
+				
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				throw new InputMisMatchException();
+			}
+	}
+	
+	
 
 }
